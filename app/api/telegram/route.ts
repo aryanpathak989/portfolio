f06f321db9aab30a1fbdd213826f1d7b9cb2042c
aryanpathak989 <aryanpathak738@gmail.com>
2025-07-12 16:14:37 +0530

    saving changes

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -1,6 +1,8 @@
 // File: app/api/telegram/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = 'edge';
+
 export async function POST(req: NextRequest) {
   const data = await req.json();
   const { firstName, email, projectBudget, aboutProject } = data;
